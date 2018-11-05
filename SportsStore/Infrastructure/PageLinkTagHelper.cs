0d441dc80aa243640c3b16b24e623fc74725ef32
tangqin <tangqintang@outlook.com>
2018-11-05 18:16:37 +0800

    chapter8

---
+++ SportsStore/Infrastructure/PageLinkTagHelper.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-model")]
+    public class PageLinkTagHelper : TagHelper
+    {
+        private IUrlHelperFactory urlHelperFactory;
+
+        public PageLinkTagHelper(IUrlHelperFactory helperFactory)
+        {
+            urlHelperFactory = helperFactory;
+        }
+    }
+}
